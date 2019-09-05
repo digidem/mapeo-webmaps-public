73ead39419e1c775944aaef6ff729f60665e16c4
Gregor MacLennan <gmaclennan@digital-democracy.org>
2019-09-05 20:26:11 +0100

    fix: Fix sw cache for pages

--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -4,8 +4,16 @@ module.exports = {
     '**/*.{js,json,jpg,html,png,css}'
   ],
   'swDest': 'dist/public/sw.js',
+  'cacheId': 'mapeo-public-cache',
   'skipWaiting': true,
   'runtimeCaching': [{
+    urlPattern: /public/,
+    handler: 'staleWhileRevalidate',
+    options: {
+      cacheName: 'mapeo-public-cache',
+      cacheableResponse: {statuses: [0, 200]}
+    }
+  }, {
     // You can use a RegExp as the pattern:
     urlPattern: /https:\/\/api\.mapbox\.com|https:\/\/[abcd]\.tiles\.mapbox\.com|https:\/\/ecn\.t\d\.tiles\.virtualearth\.net/,
     handler: 'staleWhileRevalidate',
