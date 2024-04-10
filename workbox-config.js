57acb4cbdb014cd85a357fe248e289d6a3c474d9
Gregor MacLennan <gmaclennan@digital-democracy.org>
2024-04-10 14:38:24 +0100

    fix strategy name

--- workbox-config.js
+++ workbox-config.js
@@ -16,7 +16,7 @@ module.exports = {
     {
       urlPattern:
         /https:\/\/firestore.googleapis.com|https:\/\/firebasestorage.googleapis.com/,
-      handler: 'staleWhileRevalidate',
+      handler: 'StaleWhileRevalidate',
       options: {
         cacheName: 'dynamic',
         cacheableResponse: { statuses: [0, 200] }
@@ -26,7 +26,7 @@ module.exports = {
       // You can use a RegExp as the pattern:
       urlPattern:
         /https:\/\/api\.mapbox\.com|https:\/\/[abcd]\.tiles\.mapbox\.com|https:\/\/ecn\.t\d\.tiles\.virtualearth\.net/,
-      handler: 'staleWhileRevalidate',
+      handler: 'StaleWhileRevalidate',
       // Any options provided will be used when
       // creating the caching strategy.
       options: {
