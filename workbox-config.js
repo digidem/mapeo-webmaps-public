b4426fbd6706d7637ec253aecb656c077ba654af
Gregor MacLennan <gmaclennan@digital-democracy.org>
2024-04-10 12:06:45 +0100

    don't cache firebase or mapbox in SW

--- workbox-config.js
+++ workbox-config.js
@@ -12,28 +12,7 @@ module.exports = {
         cacheName: 'static',
         cacheableResponse: { statuses: [0, 200] }
       }
-    },
-    {
-      urlPattern:
-        /https:\/\/firestore.googleapis.com|https:\/\/firebasestorage.googleapis.com/,
-      handler: 'StaleWhileRevalidate',
-      options: {
-        cacheName: 'dynamic',
-        cacheableResponse: { statuses: [0, 200] }
-      }
-    },
-    {
-      // You can use a RegExp as the pattern:
-      urlPattern:
-        /https:\/\/api\.mapbox\.com|https:\/\/[abcd]\.tiles\.mapbox\.com|https:\/\/ecn\.t\d\.tiles\.virtualearth\.net/,
-      handler: 'StaleWhileRevalidate',
-      // Any options provided will be used when
-      // creating the caching strategy.
-      options: {
-        cacheName: 'map',
-        cacheableResponse: { statuses: [0, 200] }
-      }
     }
   ],
-  globIgnores: ['../workbox-cli-config.js']
+  globIgnores: ['../workbox-config.js']
 }
